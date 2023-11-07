1a18b3f93db8d43919782966a8bf61c074f70449
Ali Benhenia <abenheni@e1r7p7.1337.ma>
2023-11-07 15:05:03 +0100

    empty array

---
+++ check_object.js
@@ -0,0 +1,8 @@
+// check object is array or not
+const arr = [1,2,3,4,4];
+const obj = {}
+
+if (Array.isArray(arr))
+    console.log("done")
+if (Number.isInteger(33))
+    console.log("num")
---
+++ empty_array.js
@@ -0,0 +1,5 @@
+// empty arry
+let tmp = [];
+let arr = [1,2,3];
+arr = [];
+console.log(arr)
