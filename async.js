f09a6111902b3e1d805b689d899a491b8bf1cb15
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-22 12:05:05 +0000

    callback function.

--- async.js
+++ async.js
@@ -9,3 +9,29 @@ const getSome = (a,b,callBack)=>{
 }
 
 console.log(getSome(4,5,printSome));
+// Note
+// When you pass a function as an argument, remember not to use parenthesis.
+
+// Right: myCalculator(5, 5, myDisplayer);
+
+// Wrong: myCalculator(5, 5, myDisplayer());
+
+
+// exmples
+const nums = [1,2,3,4,5,11,33,44,545,55,0,-11,8,3,464];
+
+const filterNums = (nums,callBack)=>{// callBack return bool
+    let i = 0;
+    const newData = []
+    while (i < nums.length )// -1 for last ele
+    {
+        if (callBack(nums[i]))
+            newData.push(nums[i]);
+        i++;
+    }
+    return (newData);
+}
+
+console.log(filterNums(nums,(x)=> x <= 10));//callback function
+
+
