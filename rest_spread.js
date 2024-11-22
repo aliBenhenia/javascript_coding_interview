4a60990ea507eb4fdae5494d859ddf9626587e45
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-22 17:14:27 +0000

    rest

--- rest_spread.js
+++ rest_spread.js
@@ -9,3 +9,12 @@ const print = (...rest)=>{
 const numbers = [1, 2, 3];
 const moreNumbers = [...numbers, 4, 5]; // Spread elements into a new array
 console.log(moreNumbers); // [1, 2, 3, 4, 5]
+// 1. Optional Chaining (?.) Operator
+// The optional chaining operator (?.) allows you to safely access properties or call methods on an object that might be null or undefined, without throwing an error. Instead of manually checking if a value exists before accessing it, optional chaining automatically returns undefined if a reference is null or undefined at any point in the chain.
+
+// Syntax
+// javascript
+// Copy code
+// object?.property
+// object?.method()
+// object?.[key]
