883fd75418385f383e4a9a0d947501cdde1353db
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-22 15:48:24 +0000

    closure

--- Closures.js
+++ Closures.js
@@ -54,3 +54,24 @@ function outerFunction() {
 const myClosure = outerFunction(); // outerFunction runs and returns innerFunction
 myClosure(); // Output: 1 (innerFunction remembers `count`)
 myClosure(); // Output: 2 (it still remembers `count`)
+
+//Example 2: Using Closures to Create Private Variables
+function createCounter() {
+    let count = 0; // Private variable
+
+    return {
+        increment: function () {
+            count++;
+            console.log(count);
+        },
+        reset: function () {
+            count = 0;
+            console.log("Reset to:", count);
+        },
+    };
+}
+
+const counter = createCounter();
+counter.increment(); // Output: 1
+counter.increment(); // Output: 2
+counter.reset();     // Output: Reset to: 0
