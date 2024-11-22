dd020166fff9dc600b11802a20c8bdf653249e34
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-22 17:09:40 +0000

    rest

--- rest_spread.js
+++ rest_spread.js
@@ -3,4 +3,9 @@ const print = (...rest)=>{
     
 }
 
-print(1,2,3,100,8)
+// print(1,2,3,100,8)
+
+
+const numbers = [1, 2, 3];
+const moreNumbers = [...numbers, 4, 5]; // Spread elements into a new array
+console.log(moreNumbers); // [1, 2, 3, 4, 5]
