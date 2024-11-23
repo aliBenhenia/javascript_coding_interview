d4601297ddeee505008c7a2e84da2f641c9f4d23
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-23 16:31:53 +0000

    Palindrome Function Implementation

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,14 @@
 const checkpalindrome = (s)=>{
     let  str = s.toLowerCase();
     let rev = str.split("").reverse().join("");
+    //Strings are immutable in JavaScript
+    //To use reverse(), we must first convert the string into an array of its individual characters. The split('') method achieves this by breaking the string into an array of characters.
     return (str === rev);
 }
 
-console.log(checkpalindrome("123321"));
+// console.log(checkpalindrome("123321"));
+let cc = "hello bro";
+
+cc.split("").at(0) = '1'
+console.log(cc)
 
