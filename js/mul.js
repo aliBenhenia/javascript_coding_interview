91e00525dbc2b099868d2f6f4a78948e8f0becee
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-25 13:46:06 +0000

    mull

--- js/mul.js
+++ js/mul.js
@@ -1,10 +1,10 @@
 function mul(n)
 {
-   return ( function(n1)
+   return ( function(n1)// anonymous function returned and child has access to outer func
     {
-        return ( function(n2)
+        return ( function(n2)// anonymous function returned and child has access to outer func
             {
-                return (n * n1 * n2);
+                return (n * n1 * n2);// anonymous function returned and child has access to outer func
             })
     })
 }
