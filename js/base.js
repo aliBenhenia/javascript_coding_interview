595144be4d5e048d3bcb4cd60ac3b90f076f106e
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-25 13:51:07 +0000

    qdew

---
+++ js/base.js
@@ -0,0 +1,10 @@
+const createBase = (n)=>{
+    let base = n;
+    return ((add)=>{
+        return (add + n);
+    })
+}
+
+const addSix = createBase(6);
+console.log(addSix(10))
+console.log(addSix(21))
