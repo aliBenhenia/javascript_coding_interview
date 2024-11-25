423584d19b5182c73f4ad8a84ed6c12a831f0f8a
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-25 14:40:58 +0000

    non-bulewan

---
+++ js/non-bulean.js
@@ -0,0 +1,21 @@
+const checkNonbulean = (data)=>{
+    if (data)
+        return (true);
+    return false;
+}
+
+console.log(checkNonbulean(1));
+console.log(checkNonbulean("qwfwef"));
+console.log(checkNonbulean(23.233));
+console.log(checkNonbulean(true));
+console.log(checkNonbulean({a : 1}));
+console.log(checkNonbulean([1,2,23,3]));
+console.log(checkNonbulean(new Date()));
+console.log(checkNonbulean(new Map()));
+console.log(checkNonbulean(new Set()));
+console.log(checkNonbulean([]));
+console.log(checkNonbulean({}));
+console.log(checkNonbulean(0));
+console.log(checkNonbulean());// undifend
+console.log(checkNonbulean(undefined));
+console.log(checkNonbulean(null));
