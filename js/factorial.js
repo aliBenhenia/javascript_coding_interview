b721cdcaeccafdf2f5c518e18e094364f382e3da
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-25 15:37:02 +0000

    ewqFEW

--- js/factorial.js
+++ js/factorial.js
@@ -1,7 +1,14 @@
-const factorial = (n)=>{
-    if (n == 0) // base case
-        return 1;
-    return (factorial(n - 1) * n);
+// const factorial = (n)=>{
+//     if (n == 0) // base case
+//         return 1;
+//     return (factorial(n - 1) * n);
+// }
+// // https://www.keka.com/javascript-coding-interview-questions-and-answers
+// console.log(factorial(5))
+
+const dd = (s)=>{
+    let cc = s.split("").reverse().join("");
+    return (s === cc);
 }
 
-console.log(factorial(5))
+console.log(dd("mada"))
