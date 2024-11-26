72060f1f2cce14819f0297d1b0348b8ee8a0e1f5
Ali benhenia <95689141+aliBenhenia@users.noreply.github.com>
2024-11-26 16:49:30 +0000

    ewd

--- ts/generics.ts
+++ ts/generics.ts
@@ -1,3 +1,8 @@
+interface storeType{
+    model : number,
+    price : number
+}
+
 interface userProps<T>
 {
     data : T[],
@@ -8,8 +13,31 @@ const user:userProps<number> = {
     data : [1,2,3,4],
     name : "ali"
 };
+const user1:userProps<string> = {
+    data : ["1,2,3,4","dd","ded"],
+    name : "ali"
+};
+const user2:userProps<boolean> = {
+    data : [true,false],
+    name : "ali"
+};
+const user3:userProps<storeType> = {
+    data : [{model : 1, price : 33}],
+    name : "ali"
+};
+
+
+
 
 console.log(user.data[0])
 
+
+const printer = <T>(arg: T)=>{
+    console.log(arg)
+}
+
+printer(1);
+printer("wd");
+printer(user1);
 
 
