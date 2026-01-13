b6be8076b648b652b3ae2575bdb8bf80752f4989
Ali Benhenia <alibenhenia1@gmail.com>
2026-01-12 23:56:09 -0500

    anagaram

--- js_coding_problems/test.js
+++ js_coding_problems/test.js
@@ -9,3 +9,33 @@ Promise.resolve().then(() => {
 });
 
 console.log("End");
+
+
+// Call Stack (Desk)
+// +-----------------+
+// | console.log     |
+// +-----------------+
+// | main function   |
+// +-----------------+
+
+// Web APIs (Helpers)
+// +-----------------+
+// | setTimeout 0ms  |
+// | fetch()         |
+// +-----------------+
+
+// Task Queue (Inbox)
+// +-----------------+
+// | setTimeout cb   |
+// +-----------------+
+
+// Microtask Queue (Urgent Inbox)
+// +-----------------+
+// | Promise cb      |
+// +-----------------+
+
+// Event Loop (Mailman)
+// 1. Check stack
+// 2. If empty, take microtask first
+// 3. Then take macrotask
+
