47d603925a632c9129d0b5c5bb89c986184be29b
Ali Benhenia <alibenhenia1@gmail.com>
2026-01-26 16:47:25 +0100

    ClosureCounter

--- js_coding_problems/ClosureCounter.js
+++ js_coding_problems/ClosureCounter.js
@@ -10,3 +10,25 @@ const saved = ClosureCounter(55);
 console.log(saved.increment())
 console.log(saved.increment())
 console.log(saved.increment())
+
+// implemneted with class
+class ClosureCounterClass {
+    constructor(init = 0)
+    {
+        let counter = init; // private variable
+        this.increment = ()=>{
+            counter++;
+        }
+        this.getCounter = ()=>{
+            return counter;
+        }
+    }
+}
+
+const counter = new ClosureCounterClass();
+counter.increment();// 1
+counter.increment();// 2
+console.log(counter.getCounter())
+counter.increment();// 3
+console.log(counter.getCounter())
+
