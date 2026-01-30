9903be5b02dcc88f261d1cf4debeb478b23ac5e5
Ali Benhenia <alibenhenia1@gmail.com>
2026-01-30 16:37:57 -0500

    custom_mapo

---
+++ js_coding_problems/map.js
@@ -0,0 +1,14 @@
+
+const custom_map = (arr,fn)=>{
+    const new_arr = [];
+    let i = 0;
+    while (i < arr.length)
+    {
+        new_arr.push(fn(arr[i]));
+        i++
+    }
+    return new_arr;
+}
+const nums = [1,2,3,4];
+const maped = custom_map(nums, ((item)=> item + 1));
+console.log(maped);
